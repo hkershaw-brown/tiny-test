9bea3b03377b50f37a1e46321f1cc4ed6bf5cc3f
Helen Kershaw <hkershaw@ucar.edu>
2020-12-10 18:06:36 -0700

    Netcdf differences

--- check_diff.sh
+++ check_diff.sh
@@ -5,10 +5,17 @@ new=mpas_test.new
 master=mpas_test.master
 manhat=mpas_test.manhatten
 
+
+# text files
 declare -a output=("obs_seq.final")
  
-# Iterate the string array using for loop
 for file in ${output[@]}; do
    diff $new/$file $master/$file 
 done
 
+# netcdf files
+declare -a output=("preassim_member_0001.nc" "preassim_mean.nc" "preassim_sd.nc" "output_sd.nc" "output_mean.nc")
+
+for file in ${output[@]}; do
+   nccmp -d $new/$file $master/$file 
+done
